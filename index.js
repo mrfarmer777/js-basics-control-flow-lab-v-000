ed8550540ef5decf5eaaa793a15bee716c5df290
Matt Farmer <mrfarmer777@gmail.com>
2018-07-26 05:12:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,10 +2,12 @@
 function scuberGreetingForFeet(length){
   let result;
   if(length<=400){
-     result="This one is on me";
+     result="This one is on me!";
+  } elseif(length>2500){
+    result="No can do.";
   } else {
     result="I will gladly take your thirty bucks.";
-  }
+  } 
   return result;
   
 }
