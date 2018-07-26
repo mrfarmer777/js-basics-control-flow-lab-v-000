3afa8a0c578c18e0f2b89d78425a15bccf5ac5ca
Matt Farmer <mrfarmer777@gmail.com>
2018-07-26 05:11:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 // Write your code in this file!
 function scuberGreetingForFeet(length){
-  length>=400;
+  let result;
+  if(length>=400){
+     result="This one is on me";
+  } else {
+    result="I will gladly take your thirty bucks.";
+  }
+  return result;
+  
 }
